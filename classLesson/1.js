ed6e0b232de0b9ecc8d6426d5de40d25027452ce
fatihyanik <fatihyanik07@gmail.com>
2021-09-05 10:22:02 +0200

    I added 1.js

---
+++ classLesson/1.js
@@ -0,0 +1,60 @@
+//console.log("Hi, there!");
+
+// expressions, values, storing values in variables.
+// console.log("James Bond");
+// let, const, var
+// "="_ assignment operator
+// assignment statement : assigns the value of the expression on the right to the variable on the left
+// 1. declares the variable "myName", using the "let" keyword
+// 2. it assigns the variable "myName" the value of the expression "James Bond": "James Bond";
+// 1. declare a let variable
+// 2. assign the variable the value "James Bond" (initialize the variable with the value "James Bond")
+
+/*
+let myName = "James Bond";
+console.log(myName);
+*/
+// variable reassignment
+/*myName="Dr. No";
+console.log(myName);// "Dr. No"
+*/
+
+// Updating the value of a variable in terms of its old value
+/*let balance = 100;
+balance = balance + 20;
+balance += 20;
+console.log(balance);
+*/
+
+/*
+let num=10;
+num='eight';
+console.log(typeof num);//string
+*/
+
+/*
+let it="b";
+console.log(it);
+let them="eat cake";
+it=them;
+console.log(it);//eat cake
+console.log(them);//eat cake
+*/
+
+/*
+var it1="b";
+console.log(it1);
+var them1="eat cake";
+it1=them1;
+console.log(it1);//eat cake
+console.log(them1);//eat cake
+*/
+
+/*
+const it2="b";
+console.log(it2);
+const them2="eat cake";
+it2=them2;//TypeError: Assignment to constant variable.
+console.log(it2);
+console.log(them2);
+*/
