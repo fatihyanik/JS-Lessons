7d465f986ec975640d082564b2bdd49946209ab5
fatihyanik <fatihyanik07@gmail.com>
2021-09-07 16:43:13 +0200

    I added implicitly and explicitly converting strings to numbers

---
+++ classLesson/8.js
@@ -0,0 +1,46 @@
+//pre-increment and post-increment operator
+//let count = 1;
+//console.log(count++);//1 returns count, then increments count by 1
+//console.log(count);//2
+//console.log(++count);//3 increments count by 1 and returns the incremented count
+
+//the decrement operator "--" works in the same way
+//let count = 10;
+//console.log(count--);//10 returns count, then decrements count by 1
+//console.log(count);//9
+//console.log(--count);//8 decrements count by 1 and returns the incremented count
+
+//console.log(4); //integers
+//console.log(4.1);//floating point number (floats)
+
+//implicitly converting strings to numbers
+//console.log(typeof(1 * '3'));//implicit type conversion / type coercion 
+//console.log(+'3');//implicitly converts string to number
+//console.log(1 * '3');//implicit type conversion / type coercion
+
+//explicitly converting strings to numbers
+//let str = '3';
+//console.log(parseInt(str));
+//console.log(parseInt('3.9'));
+//console.log(parseFloat('3.9'));
+//console.log(Number('3.9'));
+
+//Create two variables. One variable should contain a string value and the other should contain a number. Concatenate the string and the number.
+//let introSentence = "hello, my name is Fran and I am";
+//let age = 25;
+//console.log(introSentence+" " +age);//string
+
+//Create a variable with the value of “1005”. Convert it to a number.
+//let value = "1005";
+//console.log(parseInt(value));
+//console.log(Number(value));
+//console.log(1 * value);
+//console.log(+ value);
+
+//Create a variable with the value of “10.05". Convert it to a number.
+//let num = "10.05";
+//console.log(parseFloat(num));
+//console.log(parseInt(num));
+//console.log(Number(num));
+//console.log(1 * num);
+//console.log(+ num);
