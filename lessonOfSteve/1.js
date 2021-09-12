d14c0a883ab75477ab96a018ed51ce0a069711d3
fatihyanik <fatihyanik07@gmail.com>
2021-09-12 09:21:22 +0200

    I added first lesson

---
+++ lessonOfSteve/1.js
@@ -0,0 +1,17 @@
+//tell the computer to remember something
+//DECLARING & ASSIGNING
+var name = "Steve"; //Datatype String
+var id = 234567; //Datatype Number
+var alive = true; //Datatype Boolean
+var xyz;
+
+//Overwriting Assigning new values
+alive = false;
+id = 7;
+name = "Fatih";
+xyz = null;
+
+console.log(name);
+console.log(id);
+console.log(alive);
+console.log(xyz);
