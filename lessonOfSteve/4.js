13a2ca4ab540e27ca2d7fd19971b93a57f1f49dd
fatihyanik <fatihyanik07@gmail.com>
2021-09-12 09:38:28 +0200

    I added 4.lesson

---
+++ lessonOfSteve/4.js
@@ -0,0 +1,27 @@
+var first_name = "Ricky";
+var last_name = "Bobby";
+var age = 45;
+var first = true;
+var last = false;
+var bob;
+
+//Falsey
+//false, null, undefined, 0, "", '', NaN
+
+if(age <= 40){
+    //if the value of age is less than or equal to 40
+}else if ((age>40) && (age<50)){
+    // if age is greater than 40
+    // && -means AND
+    // || -means OR
+}else if(age<0){
+    //otherwise prevent something from happening
+}else{
+    //greater than 50
+}
+
+//COMPARISON operators
+// < <= > >= == != ! === 
+if(!first){
+    
+}
