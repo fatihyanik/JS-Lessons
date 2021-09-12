063703db604b1dce4b1c0fb3720a6b5b5e20cbfc
fatihyanik <fatihyanik07@gmail.com>
2021-09-12 09:32:53 +0200

    I added 3.lesson

---
+++ lessonOfSteve/3.js
@@ -0,0 +1,19 @@
+var first_name = "Ricky";
+var last_name = "Bobby";
+var age = 45;
+var first = true;
+var last = false;
+var bob;
+
+//Falsey
+//false, null, undefined, 0, "", '', NaN
+
+if(age){
+    
+}
+
+if(age == 45){
+    //do something if true
+}else{
+    //do something if false
+}
