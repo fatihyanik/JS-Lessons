0afa988140a5c3ebae7abafc67b1ea40fbe539a5
fatihyanik <fatihyanik07@gmail.com>
2021-09-15 19:53:52 +0200

    I added 22.js

---
+++ lessonOfSteve/22.js
@@ -0,0 +1,18 @@
+//Globals and Numbers and Math. 
+
+var r = parseInt("234.234");
+console.log(r);
+var r1 = parseInt("As234.234");
+console.log(r1);
+
+let num = "234.23423423";
+num1 =Number.parseInt(num);
+
+console.log(num1);
+
+console.log(Math.round(7.66666));
+console.log(Math.floor(7.888888));
+console.log(Math.ceil(7.888888));
+//console.log(Math.random());
+console.log(Math.max(97,43,112));
+
